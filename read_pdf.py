d567134029cb2310852ae433ecf7c78bce098147
nielson.trung <nielson.trung@ucalgary.ca>
2021-04-17 16:48:02 -0600

    refactored read_pdf.py

--- read_pdf.py
+++ read_pdf.py
@@ -1,40 +1,68 @@
 import os, json
 from tika import parser
-# extract text from estatment
-fname = './451029XXXXXX6910-2016-01-12-2016-02-12.pdf'
-# extract the date from the file name
-i = fname.find('-')
-account = fname[2:i]
-year = fname[i+1:i+5]
-i = fname.find(year+'-')+5
-start_month = fname[i:i+2]
-i = fname.rfind(year+'-')+5
-end_month = fname[i:i+2]
-  
-raw = parser.from_file(fname)
-content = raw['content']
-# write pdf content to raw.txt
-f = open('raw.txt','w')
-f.write(content)
-f.close()
-lines = content.splitlines()
 
+# extract text from estatment
+def get_pdf_content(fname):
+  raw = parser.from_file(fname)
+  content = raw['content']
+  return content
+
+def get_account_number(fname):
+  return fname[:fname.find('-')]
+
+# extract the year, start month, and end month from the file name
+def get_year_months(fname):
+  i = fname.find('-')
+  account = fname[2:i]
+  year = fname[i+1:i+5]
+  i = fname.find(year+'-')+5
+  start_month = fname[i:i+2]
+  i = fname.rfind(year+'-')+5
+  end_month = fname[i:i+2]
+  return [year, start_month, end_month]
+
+def get_month_name(n):
+  months ={
+    "01": "Jan",
+    "02": "Feb",
+    "03": "Mar",
+    "04": "Apr",
+    "05": "May",
+    "06": "Jun",
+    "07": "Jul",
+    "08": "Aug",
+    "09": "Sep",
+    "10": "Oct",
+    "11": "Nov",
+    "12": "Dec"
+  }
+  return months[n].upper()
+
+# remove all occurrences of the month
 def remove_all_occurrences(s,old,new):
   temp = s
   while temp.find(old) >= 0:
     temp = s.replace(old, new)
   return temp
 
+# return a business category from the business details
 def get_category(business):
-
   category = ''
   return category
 
-def get_data(f,t,lines):
-  f.write('id, transaction date, posting date, business, amount\n')
-  t.write('id, transaction date, posting date, business, amount\n')  
+# 
+def get_data(fname, f, lines):
+  csv_header = 'account number, transaction id, transaction date, posting date, business, amount\n'
+  f.write(csv_header)
   # prepare json data
   data = {}
+  # get account number from the file name
+  account_number = get_account_number(fname) + ','
+  # get year and month of transaction
+  year_months = get_year_months(fname)
+  year = year_months[0]
+  start_month = year_months[1]
+  end_month = year_months[2]
   # maintenance transactions
   maintenance_transactions = []
   maintenance_amounts = []
@@ -85,27 +113,27 @@ def get_data(f,t,lines):
     elif line.isnumeric():
       id = line + ','
       ids.append(id)
-      transaction = id + transaction
+      transaction = account_number + id + transaction
     # get transaction amount
     elif (transaction.find(start_month) != -1 or transaction.find(end_month) != -1):
       if line.find('$') != -1:        
-        amount = line.replace("$",'')        
+        amount = line.replace("$",'')
         transaction += amount + '\n'
         transactions.append(transaction)
         f.write(transaction)
-        t.write(transaction)
         transaction = ''
-        # get the amount as a float
+        # get the amount as a float     
         amount = float(amount)
         amounts.append(amount)
   data = [transactions, businesses, amounts]
   return data
+
+# 
+fname = '451029XXXXXX6910-2016-01-12-2016-02-12.pdf'
 # prepare file names for csv and txt file
 csv_file = fname.replace("pdf","csv")
-txt_file = fname.replace("pdf","txt")
 # open files for writing
 f = open(csv_file,"w")
-t = open(txt_file,"w")
-data = get_data(f,t,lines)
+lines = get_pdf_content(fname).splitlines()
+data = get_data(fname,f,lines)
 f.close()
-t.close()
