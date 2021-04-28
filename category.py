064d7add6400b5aa990186770309107495adbf74
nielson.trung <nielson.trung@ucalgary.ca>
2021-04-28 16:43:15 -0600

    updating files

---
+++ category.py
@@ -0,0 +1,50 @@
+from re import search
+# get a category from transaction description 
+# category listings are based from my own personal transaction history
+def get_category(description):
+  automotive = ['acura', 'auto', 'canadian tire', 'cdn tire', 'compu-care', 'mitsubishi']
+  clothing = ['abercrombie', 'h&m', 'old navy', 'shoe', 'simons', 'skechers', 'sport chek', 'uniqlo', 'urban planet', 'winners', 'zara']
+  education = ['cbe', 'john wiley & sons', 'quizlet', 'sait', 'u of c', 'unversity', 'vitalsource']
+  electronics = ['best buy', 'logitech', 'memory', 'microsoft', 'newegg', 'source']
+  entertainment = ['cineplex', 'entertainment', 'games', 'jagex', 'mojang', 'spotify', 'stampede', 'steam']
+  fee = ['fee']
+  gas = ['eleven', 'esso', 'gas', 'husk', 'petro', 'shell']
+  general = ['aliexpress', 'amazon', 'dollar store', 'miniso', 'staples', 'wal-mart']
+  groceries = ['atlantic superstore', 'bulk barn', 'co-op', 'costco', 'dominion', 'extra foods', 'farm boy', 'food basics', 'fortinos', 'freshco', 'freshmart', 'grocery', 'iga', 'loblaws', 'loeb', "longo's", 'market', 'maxi', 'metro', "mike dean's super food stores", 'no frills', 'provigo', 'quality foods', 'rabba', 'real canadian superstore', 'safeway', 'save-on-foods', 'saveeasy', 'sobeys', "steinberg's (supermarket)", 'superstore', 'thrifty foods', 'valu-mart', 'independent grocer', 'zehrs markets']
+  healthcare = ['ahs', 'brunet', 'dental', 'drug', 'familiprix', 'jean coutu group', 'lawtons', 'london drugs', 'pharmachoice', 'pharmacy', 'rexall', 'shoppers drug mart']
+  homeimprovement = ['lowe', 'rona', 'the home depot']
+  loans = ['loan']
+  mobile =  ['bell', 'fido', 'koodo', 'mobile', 'rogers', 'telus']
+  other = ['other']
+  restaurants = ['a&w', 'bbq', 'beef', 'black bull', 'brewsters', 'cafe', 'chatime', 'chef', 'chef', 'chianti', 'chicken', 'chinese', 'coco tea', 'dairy', 'deli', "denny's", 'donday', 'doordash', 'food', 'grill', 'kitchen', 'korean', 'marble', 'mcdonald', 'pho', 'pizza', 'ramen', 'restaurant', 'rotisserie', 'saigon', 'samosa', 'shawarma', 'ssome', 'sushi', 'tea', 'thai', 'vietnam', "wendy's", 'wok']
+  travel = ['airport', 'bus', 'park', 'transit']
+  utilities = ['cable', 'enmax', 'insurance']
+
+  categories = {
+    'automotive': automotive,
+    'clothing': clothing,
+    'education': education,
+    'electronics': electronics,
+    'entertainment': entertainment,
+    'fee': fee,
+    'gas': gas,
+    'general merchandise': general,
+    'groceries': groceries,
+    'healthcare': healthcare,
+    'home improvement': homeimprovement,
+    'loans': loans,
+    'mobile': mobile,
+    'other': other,
+    'restaurants': restaurants,
+    'travel': travel,
+    'utilities': utilities
+  }  
+
+  category = ''
+  for i in categories:
+    for j in categories[i]:
+      if search(j, description):
+        category = i
+        return category
+  category = 'other'  
+  return category
--- rename_file.py
+++ rename_file.py
@@ -1,37 +1,32 @@
-import os
-import sys
+import os, sys
 
-# print(sys.argv)
+months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
-# if len(sys.argv) == 2:
-    
-fnames = os.listdir('.')
-
-l = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-
-months = {
-"Jan": "-01-",
-"Feb": "-02-",
-"Mar": "-03-",
-"Apr": "-04-",
-"May": "-05-",
-"Jun": "-06-",
-"Jul": "-07-",
-"Aug": "-08-",
-"Sep": "-09-",
-"Oct": "-10-",
-"Nov": "-11-",
-"Dec": "-12-"
+months_dict = {
+  "Jan": "-01-",
+  "Feb": "-02-",
+  "Mar": "-03-",
+  "Apr": "-04-",
+  "May": "-05-",
+  "Jun": "-06-",
+  "Jul": "-07-",
+  "Aug": "-08-",
+  "Sep": "-09-",
+  "Oct": "-10-",
+  "Nov": "-11-",
+  "Dec": "-12-"
 }
 
-# s = "451029XXXXXX6910-2016Dec13-2017Jan12.pdf"
-# print(s.replace("Dec",months["Dec"]))
-s = ''
-for fname in fnames:
-  s = fname
-  print(s)
-  for month in l:
-    if s.find(month) != -1:
-      s = s.replace(month, months[month])
-  print(s)
-  os.rename(fname, s)
+# renames a single file
+def rename_file(fname):
+  for month in months:
+    if fname.find(month) != -1:
+      new_name = fname.replace(month, months_dict[month])
+    os.rename(fname, new_name)
+  return new_name
+
+# renames all files given a path
+def rename_files(path):
+  fnames = os.listdir(path)
+  for fname in fnames:
+    rename_file(fname)
